c32d9205d967ef566c8a0f97686236920635d016
Gihan Marasingha <gihan.marasingha@gmail.com>
2023-10-09 19:11:58 +0000

    restore package-lock.json update install script

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 ## This script installs Lean and the Lean Game Maker
 
+cwd=$(pwd) # store current working directory
+
 echo "Installing Lean..."
-curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | sh
+curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf \
+  | sh -s -- --default-toolchain none --no-modify-path -y # Install the elan Lean version manager
 cd ${HOME}/.elan
 chmod u+x env
 ./env
@@ -13,20 +16,24 @@ echo "Lean installed."
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
+sudo apt update
+
 # Now get a few important packages.
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
 export PATH="$HOME/.pyenv/bin:$PATH"
 echo "Installing Python. Please wait ..." # Install a local version of Python 3.7.2
 pyenv install 3.7.2
 echo "Python installed"
-sudo apt install python3-pip # Ensure we have pip
+sudo apt install -y python3-pip # Ensure we have pip
 
 # Now we'll set up a virtual environment for compiling the Lean Game Maker. First get things ready.
 sudo -H pip3 install virtualenvwrapper
 
 
-cwd
+cd ${cwd}
 
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=$(which python3)  # The result of 'which python3' is /usr/local/python/current/bin/python3 in GitHub codespaces
 # Run this script to build and install the Lean Game Maker
 source virtualenvwrapper.sh
 mkvirtualenv --python=$HOME/.pyenv/versions/3.7.2/bin/python lean_env
